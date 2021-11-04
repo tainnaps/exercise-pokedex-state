6a11dd72992dedcf222f9e787bfc92a1581ab6d6
Tainá Santana <tainnaps@gmail.com>
2021-11-03 22:08:13 -0300

    Exercicio 1 - adequa pagina para renderizar um pokemon a cada click

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      index: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((previousState) => {
+      return previousState.index === (pokemons.length - 1) ? {  index: 0 } : {  index: previousState.index + 1 };
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Pokedex pokemon={pokemons[this.state.index]} />
+        <button onClick={this.handleClick} className="next-btn">Próximo</button>
+      </div>
+    );
+  }
 }
 
 export default App;
